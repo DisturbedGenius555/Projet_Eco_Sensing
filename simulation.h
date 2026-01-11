63ca3a3798b24bb32a35f4fa2111b58d84e50851
mark-olf <fleury.kassa1@gmail.com>
2026-01-11 20:32:22 +0100

    Add header file for simulation functions

---
+++ simulation.h
@@ -0,0 +1,9 @@
+#ifndef SIMULATION_H_INCLUDED
+#define SIMULATION_H_INCLUDED
+
+#include "structures.h"
+
+int attempt_transmission(Capteur *c);
+float calculer_energie_transmission(float distance);
+#endif // SIMULATION_H_INCLUDED
+
