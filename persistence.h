e5a5214dcc64bfcef7cb44085b340276e74f8d3f
mark-olf <fleury.kassa1@gmail.com>
2026-01-11 20:33:09 +0100

    Add persistence header for saving and loading state

---
+++ persistence.h
@@ -0,0 +1,10 @@
+#ifndef PERSISTENCE_H_INCLUDED
+#define PERSISTENCE_H_INCLUDED
+
+#include "structures.h"
+
+void sauvegarder_etat(Capteur *c);
+void charger_etat(Capteur *c);
+
+
+#endif // PERSISTENCE_H_INCLUDED
